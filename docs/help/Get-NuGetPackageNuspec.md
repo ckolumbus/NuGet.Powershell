c61d846dcae43f02f900ceef3a9a4e1c31f94aee
Chris Drexler <ckolumbus@ac-drexler.de>
2024-03-22 15:08:17 +0100

    feat: :sparkles: new Get-NugetPackageNuspec cmdlet
    for local an remote packages

---
+++ docs/help/Get-NuGetPackageNuspec.md
@@ -0,0 +1,249 @@
+---
+external help file: NuGet.PowerShell.dll-Help.xml
+Module Name: NuGet.PowerShell
+online version:
+schema: 2.0.0
+---
+
+# Get-NuGetPackageNuspec
+
+## SYNOPSIS
+Get the `nuspec`s for provided package identities as `XmlDocument`.
+
+Can read from local files and remote packages. For the latter the packages needs
+to be downloaded.
+
+## SYNTAX
+
+### Path
+```
+Get-NuGetPackageNuspec [-Path] <String[]> [-ProgressAction <ActionPreference>] [<CommonParameters>]
+```
+
+### Object
+```
+Get-NuGetPackageNuspec [-PackageIdentity] <PackageIdentity[]> [-ProgressAction <ActionPreference>]
+ [<CommonParameters>]
+```
+
+### Object-ConfigFile
+```
+Get-NuGetPackageNuspec [-PackageIdentity] <PackageIdentity[]> -ConfigFile <String>
+ [-ProgressAction <ActionPreference>] [<CommonParameters>]
+```
+
+### Object-ConfigArgs
+```
+Get-NuGetPackageNuspec [-PackageIdentity] <PackageIdentity[]> -Source <String> [-SourceProtocolVersion <Int32>]
+ [-SourceCredential <PSCredential>] [-ProgressAction <ActionPreference>] [<CommonParameters>]
+```
+
+### Args
+```
+Get-NuGetPackageNuspec [-Id] <String> [-Version] <String> [-ProgressAction <ActionPreference>]
+ [<CommonParameters>]
+```
+
+### Args-ConfigFile
+```
+Get-NuGetPackageNuspec [-Id] <String> [-Version] <String> -ConfigFile <String>
+ [-ProgressAction <ActionPreference>] [<CommonParameters>]
+```
+
+### Args-ConfigArgs
+```
+Get-NuGetPackageNuspec [-Id] <String> [-Version] <String> -Source <String> [-SourceProtocolVersion <Int32>]
+ [-SourceCredential <PSCredential>] [-ProgressAction <ActionPreference>] [<CommonParameters>]
+```
+
+### ConfigFile
+```
+Get-NuGetPackageNuspec [-ConfigFile <String>] [-ProgressAction <ActionPreference>] [<CommonParameters>]
+```
+
+### ConfigArgs
+```
+Get-NuGetPackageNuspec -Source <String> [-SourceProtocolVersion <Int32>] [-SourceCredential <PSCredential>]
+ [-ProgressAction <ActionPreference>] [<CommonParameters>]
+```
+
+## DESCRIPTION
+{{ Fill in the Description }}
+
+## EXAMPLES
+
+### Example 1
+```powershell
+PS C:\> {{ Add example code here }}
+```
+
+{{ Add example description here }}
+
+## PARAMETERS
+
+### -ConfigFile
+{{ Fill ConfigFile Description }}
+
+```yaml
+Type: String
+Parameter Sets: Object-ConfigFile, Args-ConfigFile
+Aliases:
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+```yaml
+Type: String
+Parameter Sets: ConfigFile
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Id
+{{ Fill Id Description }}
+
+```yaml
+Type: String
+Parameter Sets: Args, Args-ConfigFile, Args-ConfigArgs
+Aliases:
+
+Required: True
+Position: 0
+Default value: None
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### -PackageIdentity
+{{ Fill PackageIdentity Description }}
+
+```yaml
+Type: PackageIdentity[]
+Parameter Sets: Object, Object-ConfigFile, Object-ConfigArgs
+Aliases:
+
+Required: True
+Position: 0
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -Path
+{{ Fill Path Description }}
+
+```yaml
+Type: String[]
+Parameter Sets: Path
+Aliases:
+
+Required: True
+Position: 0
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -ProgressAction
+{{ Fill ProgressAction Description }}
+
+```yaml
+Type: ActionPreference
+Parameter Sets: (All)
+Aliases: proga
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Source
+{{ Fill Source Description }}
+
+```yaml
+Type: String
+Parameter Sets: Object-ConfigArgs, Args-ConfigArgs, ConfigArgs
+Aliases:
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -SourceCredential
+{{ Fill SourceCredential Description }}
+
+```yaml
+Type: PSCredential
+Parameter Sets: Object-ConfigArgs, Args-ConfigArgs, ConfigArgs
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -SourceProtocolVersion
+{{ Fill SourceProtocolVersion Description }}
+
+```yaml
+Type: Int32
+Parameter Sets: Object-ConfigArgs, Args-ConfigArgs, ConfigArgs
+Aliases:
+Accepted values: 2, 3
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Version
+{{ Fill Version Description }}
+
+```yaml
+Type: String
+Parameter Sets: Args, Args-ConfigFile, Args-ConfigArgs
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### System.String[]
+
+### NuGet.Packaging.Core.PackageIdentity[]
+
+### System.String
+
+## OUTPUTS
+
+### System.Xml.XmlDocument
+
+## NOTES
+
+## RELATED LINKS
---
+++ src/Get-NuGetPackageNuspec.cs
@@ -0,0 +1,173 @@
+/*
+   Copyright (c) Chris Drexler
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+using System.Collections.Generic;
+using System.IO;
+using System.Management.Automation;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace NuGet.PowerShell
+{
+    [Cmdlet(VerbsCommon.Get, "NuGetPackageNuspec")]
+    [OutputType(typeof(XmlDocument))]
+    public class GetNugetPackageNuspecCmdlet : AsyncCmdlet
+    {
+        [Parameter(ParameterSetName = "Path", Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        public string[] Path { get; set; }
+
+        [Parameter(ParameterSetName = "Object", Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        [Parameter(ParameterSetName = "Object-ConfigFile", Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        [Parameter(ParameterSetName = "Object-ConfigArgs", Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        public PackageIdentity[] PackageIdentity { get; set; }
+
+        [Parameter(ParameterSetName = "Args", Mandatory = true, Position = 0, ValueFromPipelineByPropertyName = true)]
+        [Parameter(ParameterSetName = "Args-ConfigFile", Mandatory = true, Position = 0, ValueFromPipelineByPropertyName = true)]
+        [Parameter(ParameterSetName = "Args-ConfigArgs", Mandatory = true, Position = 0, ValueFromPipelineByPropertyName = true)]
+        public string Id { get; set; }
+
+        [Parameter(ParameterSetName = "Args", Mandatory = true, Position = 1, ValueFromPipelineByPropertyName = true)]
+        [Parameter(ParameterSetName = "Args-ConfigFile", Mandatory = true, Position = 1, ValueFromPipelineByPropertyName = true)]
+        [Parameter(ParameterSetName = "Args-ConfigArgs", Mandatory = true, Position = 1, ValueFromPipelineByPropertyName = true)]
+        public string Version { get; set; }
+
+        [Parameter(ParameterSetName = "ConfigFile")]
+        [Parameter(ParameterSetName = "Object-ConfigFile", Mandatory = true)]
+        [Parameter(ParameterSetName = "Args-ConfigFile", Mandatory = true)]
+        public string ConfigFile { get; set; } = "";
+
+        [Parameter(ParameterSetName = "ConfigArgs", Mandatory = true)]
+        [Parameter(ParameterSetName = "Object-ConfigArgs", Mandatory = true)]
+        [Parameter(ParameterSetName = "Args-ConfigArgs", Mandatory = true)]
+        public string Source { get; set; } = "";
+
+        [Parameter(ParameterSetName = "ConfigArgs")]
+        [Parameter(ParameterSetName = "Object-ConfigArgs")]
+        [Parameter(ParameterSetName = "Args-ConfigArgs")]
+        [ValidateSet(new string[] { "2", "3" })]
+        public int SourceProtocolVersion { get; set; } = 3;
+
+        [Parameter(ParameterSetName = "ConfigArgs")]
+        [Parameter(ParameterSetName = "Object-ConfigArgs")]
+        [Parameter(ParameterSetName = "Args-ConfigArgs")]
+        public PSCredential SourceCredential { get; set; } = PSCredential.Empty;
+
+        private List<SourceRepository> repositories = new List<SourceRepository>();
+        private SourceCacheContext cache = new SourceCacheContext();
+        private string cwd;
+
+        protected override Task BeginProcessingAsync()
+        {
+            cwd = SessionState.Path.CurrentFileSystemLocation.Path;
+            if (Source != "")
+            {
+                var rootedSource = Helpers.GetRootedPath(Source, cwd);
+                var repository = Helpers.GetRepository(rootedSource, SourceCredential.GetNetworkCredential(), SourceProtocolVersion, logger: this);
+                repositories.Add(repository);
+            }
+            else
+            {
+                var settings = Helpers.GetSettings(ConfigFile, ctx: this, logger: this);
+                var newRepositories = Helpers.GetRepositories(settings, logger: this);
+                repositories.AddRange(newRepositories);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        protected override async Task ProcessRecordAsync()
+        {
+            var cancellationToken = CancellationToken.None;
+
+            if (ParameterSetName.StartsWith("Args"))
+            {
+                PackageIdentity = new[] { new PackageIdentity(Id, NuGetVersion.Parse(Version)) };
+            }
+
+            if ((null != Path) && (Path.Length > 0))
+            {
+                foreach (var pathArg in Path)
+                {
+                    var path = Helpers.GetRootedPath(pathArg, root: cwd);
+                    WriteVerbose($"Getting NuSpec from : {path}");
+                    var packageReader = new PackageArchiveReader(path);
+
+                    if (packageReader is null)
+                    {
+                        WriteWarning($"No Package found for : {path}");
+                    }
+                    else
+                    {
+                        WriteVerbose($"Output nuspec for : {path}");
+                        var nuspecStream = await packageReader.GetNuspecAsync(cancellationToken);
+                        var xml = new XmlDocument();
+                        xml.Load(nuspecStream);
+                        WriteObject(xml);
+                    }
+                }
+            }
+            else if ((null != PackageIdentity) && (PackageIdentity.Length > 0))
+            {
+                foreach (var identity in PackageIdentity)
+                {
+                    WriteVerbose($"Searching Package for : {identity}");
+                    Stream nuspecStream = null;
+                    foreach (var repo in repositories)
+                    {
+                        WriteVerbose($"      in repo : {repo}");
+                        FindPackageByIdResource resource = await repo.GetResourceAsync<FindPackageByIdResource>();
+
+                        try
+                        {
+                            MemoryStream packageStream = new MemoryStream();
+
+                            await resource.CopyNupkgToStreamAsync(
+                                identity.Id,
+                                identity.Version,
+                                packageStream,
+                                cache,
+                                logger: this,
+                                cancellationToken);
+
+                            PackageArchiveReader packageReader = new PackageArchiveReader(packageStream);
+                            nuspecStream = await packageReader.GetNuspecAsync(cancellationToken);
+                            WriteVerbose($"Found Package {identity}");
+                            break;
+                        }
+                        catch { }
+                    }
+                    if (nuspecStream is null)
+                    {
+                        WriteWarning($"No Package found for : {identity}");
+                    }
+                    else
+                    {
+                        WriteVerbose($"Output nuspec for : {identity}");
+                        var xml = new XmlDocument();
+                        xml.Load(nuspecStream);
+                        WriteObject(xml);
+                    }
+                }
+            }
+        }
+    }
+}
