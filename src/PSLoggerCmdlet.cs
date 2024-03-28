943b297964bbe48add1879a602e5375472902f58
Chris Drexler <ckolumbus@ac-drexler.de>
2024-03-28 10:47:48 +0100

    fix: :bug: avoid exception during logging
    PSLoggerCmdlet implentation is not yet working when used with Nuget Client SDK api.
    Logging completely disabled (essentially  a "NullLogger.Instance") until a solution is found

--- src/PSLoggerCmdlet.cs
+++ src/PSLoggerCmdlet.cs
@@ -26,7 +26,7 @@ namespace NuGet.PowerShell
     public abstract class PSLoggerCmdlet : PSCmdlet, ILogger
     {
         public void Log(LogLevel level, string data) {
-            switch(level) {
+            switch (level) {
                 case LogLevel.Debug:
                     LogDebug(data);
                     break;
@@ -67,6 +67,12 @@ namespace NuGet.PowerShell
             return Task.CompletedTask;
         }
 
+        /* TODO: all logging disabled because when Nuget SDK methods actually use the logging faciltiy the following error happens
+                 and the root cause/possible solution is not understood
+                 Error: The WriteObject and WriteError methods cannot be called from outside the overrides of the
+                        BeginProcessing, ProcessRecord, and EndProcessing methods
+        */
+        /*
         public void LogDebug(string data) => WriteDebug(data);
         public void LogError(string data) => WriteError(new ErrorRecord(new Exception(data), "PSLoggerCmdlet", ErrorCategory.NotSpecified , this));
         public void LogInformation(string data) => WriteInformation(new InformationRecord(data, ""));
@@ -74,5 +80,13 @@ namespace NuGet.PowerShell
         public void LogMinimal(string data) => WriteVerbose(data);
         public void LogVerbose(string data) => WriteVerbose(data);
         public void LogWarning(string data) => WriteVerbose(data);
+        */
+        public void LogDebug(string data) { }
+        public void LogError(string data) { }
+        public void LogInformation(string data) { }
+        public void LogInformationSummary(string data) { }
+        public void LogMinimal(string data) { }
+        public void LogVerbose(string data) { }
+        public void LogWarning(string data) { }
     }
 }
